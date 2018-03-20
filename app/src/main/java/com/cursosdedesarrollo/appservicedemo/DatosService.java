1293cd4c009e1ee5f951d7c47e34642cce2dbdb2
pepesan <pepesan@gmail.com>
2018-03-20 18:31:47 +0100

    comunicacion entre el servicio y la activity

---
+++ app/src/main/java/com/cursosdedesarrollo/appservicedemo/DatosService.java
@@ -0,0 +1,46 @@
+package com.cursosdedesarrollo.appservicedemo;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import java.util.List;
+
+/**
+ * Created by pepesan on 20/3/18.
+ */
+
+public class DatosService extends IntentService {
+    public static final String ACTION_FINAL =
+            "com.cursosdedesarrollo.intent.action.FINAL";
+    private Aplicacion app;
+    private List<String> listado;
+    public DatosService() {
+        super("DatosService");
+        app=(Aplicacion)getApplication();
+        if(app!=null){
+            listado=app.getListado();
+            Log.d("app","DatosService: datos de la aplicacion: "+listado.toString());
+        }
+
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        Log.d("app","DatosService: onHandleIntent");
+        String cadenaPasada=intent.getStringExtra("cadena");
+        Log.d("app","DatosService: cadenaPasada: "+cadenaPasada);
+        tareaLarga();
+        Intent bcIntent = new Intent();
+        bcIntent.setAction(ACTION_FINAL);
+        bcIntent.putExtra("dato", cadenaPasada);
+        sendBroadcast(bcIntent);
+    }
+    private void tareaLarga()
+    {
+        try {
+            Thread.sleep(10000);
+        } catch(InterruptedException e) {}
+    }
+}
--- app/src/main/java/com/cursosdedesarrollo/appservicedemo/MainActivity.java
+++ app/src/main/java/com/cursosdedesarrollo/appservicedemo/MainActivity.java
@@ -1,5 +1,9 @@
 package com.cursosdedesarrollo.appservicedemo;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,12 +13,23 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private Aplicacion app;
     private List<String> listado;
+    private BroadcastReceiver progressReceiver=new  BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String data=intent.getStringExtra("dato");
+            Log.d("app","MainActivity: ProgressReceiver: dato: "+data);
+            //if(intent.getAction().equals(DatosService.ACTION_FINAL)) {
+                //Toast.makeText(MainActivity.this, "Tarea finalizada!", Toast.LENGTH_SHORT).show();
+            //}
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +53,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(DatosService.ACTION_FINAL);
+        registerReceiver(progressReceiver, filter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(progressReceiver);
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -52,7 +81,10 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_launch_datos_service) {
+            Intent intent = new Intent(MainActivity.this, DatosService.class);
+            intent.putExtra("cadena", "Mi cadena");
+            startService(intent);
             return true;
         }
 
